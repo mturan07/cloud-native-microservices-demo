c54e0c5238d48197f32247a4077da33edae80311
Murat Turan <46825409+mturan07@users.noreply.github.com>
2025-12-08 17:32:44 +0300

    fix: added GetEnvironmentVariable to listen on port 80

--- NotificationService/Program.cs
+++ NotificationService/Program.cs
@@ -2,7 +2,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenAnyIP(80);
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+    options.ListenAnyIP(int.Parse(port));
 });
 
 var app = builder.Build();
